1834f8ecfd7e7cfb2cf8fc7a27a7c8fc83e5bc8f
Willyelns Xavier <willyelnsg.x@gmail.com>
2022-08-03 13:46:43 -0300

    feat: add firebase init to the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
+import 'package:yuca_payment/commons/services/remote_config/firebase_remote_config.dart';
 
 import './injection_container.dart' as injection_container;
 
 import 'app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await injection_container.init();
+  await _initFirebase();
   _i18nInit();
   runApp(const App());
 }
@@ -15,4 +19,11 @@ Future<void> main() async {
 void _i18nInit() {
   initializeDateFormatting('pt_BR');
   Intl.defaultLocale = 'pt_BR';
+}
+
+Future<void> _initFirebase() async {
+  await Firebase.initializeApp();
+  final remoteConfig =
+      injection_container.serviceLocator.get<FirebaseRemoteConfigService>();
+  await remoteConfig.init();
 }
