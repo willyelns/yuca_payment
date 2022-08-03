672fe4244d97010346e633157c0030a84e258d47
Willyelns Xavier <willyelnsg.x@gmail.com>
2022-08-03 02:19:39 -0300

    feat: update app name

--- lib/app.dart
+++ lib/app.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Payment list app',
       theme: defaultAppTheme(),
       routes: routes,
     );
